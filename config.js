39444e965ee5fa017c0badc0661dcbd41d6a5866
eduardoclaudeia-sys <eduardo.claudeia@gmail.com>
2026-09-01 15:28:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 // EP FINANCE - configuração pública do Supabase
-// Publishable Key pode ficar no frontend. NUNCA coloque service_role/secret key aqui.
+// Publishable Key pode ficar no frontend.
+// NUNCA coloque service_role/secret key aqui.
+
 window.EP_CONFIG = {
   SUPABASE_URL: "https://nmrmghaarydsgqkdvinj.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_5zncFVn34v1WfyLLRaT3og_vjxwZLu0"
-  ,VAPID_PUBLIC_KEY: "BAWIv6mhITQuV1VmUYtMIeX9A_tAP9wZsriCsi0ZnuaJXmLiQTzQcJ8R8ILEp3gP_jdQXkPliIn3dTnHpFx3-Ko"
+
+  SUPABASE_PUBLISHABLE_KEY:
+    "sb_publishable_5zncFVn34v1WfyLLRaT3og_vjxwZLu0",
+
+  VAPID_PUBLIC_KEY:
+    "BAWIv6mhITQuV1VmUYtMIeX9A_tAP9wZsriCsi0ZnuaJXmLiQTzQcJ8R8ILEp3gP_jdQXkPliIn3dTnHpFx3-Ko"
+};
