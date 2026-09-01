d669220b8758a10721f880feea68fd84dce00548
eduardoclaudeia-sys <eduardo.claudeia@gmail.com>
2026-09-01 13:52:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 window.EP_CONFIG = {
   SUPABASE_URL: "https://nmrmghaarydsgqkdvinj.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_5zncFVn34v1WfyLLRaT3og_vjxwZLu0"
-  ,VAPID_PUBLIC_KEY: ""
+  ,VAPID_PUBLIC_KEY: "BIcd9hG8k8s04MBAMYU0EH-NEkQ3tZ1pUYSX5RkagW8JAuOrUu9vhTNaCmIUP38PeEyPg7eKMK8GSZGCD2Kc_Eg","privateKey":"DgC3pSs9wYisb9gERNly_gan5ZsxCRtX3gyrP1HIFCA"
 };
